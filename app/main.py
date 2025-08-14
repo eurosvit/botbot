9d64e9faa3b72103148d95d8ff150d9d3efba57c
Yana <91046756+eurosvit@users.noreply.github.com>
2025-08-14 17:50:28 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from flask import Flask, Response
 from app.logging_conf import configure_logging  # абсолютний імпорт!
 
 app = Flask(__name__)
-configure_logging(app)
+configure_logging()  # без аргументів!
 
 @app.route("/healthz")
 def healthz():
@@ -13,7 +13,6 @@ def healthz():
 def index():
     return Response("Hello, world!", mimetype="text/plain")
 
-# Додаємо запуск Flask для локального тесту (для Render/Gunicorn це не потрібно, але локально стане у пригоді)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
